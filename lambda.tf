15c6fef4ccc7da89945bc9b144f302d99c0bed3c
alexandreponte1 <alexandre.s.ponte@concrete.com.br>
2022-03-19 18:01:04 -0300

    add hash

--- lambda.tf
+++ lambda.tf
@@ -10,6 +10,7 @@ resource "aws_lambda_function" "myLambda" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "hello.lambda_handler"
   runtime       = "python3.8"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
 }
 
